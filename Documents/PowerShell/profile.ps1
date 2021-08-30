4587ce11075ee3c910f810b103a3cad546c31480
Morgan Courbet <github@sub.ploki.fr>
2021-08-30 10:22:20 +0200

    Add PowerShell config

---
+++ Documents/PowerShell/profile.ps1
@@ -0,0 +1,16 @@
+# aliases
+New-Alias -Name d -Value docker
+New-Alias -Name g -Value git
+New-Alias -Name lg -Value lazygit
+
+
+# display possible completion with fzf instead of completing
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+
+
+# starship
+Invoke-Expression (&starship init powershell)
+
+
+# zoxide
+Invoke-Expression (& { $hook = if ($PSVersionTable.PSVersion.Major -ge 6) { 'pwd' } else { 'prompt' } (zoxide init powershell --hook $hook) -join "`n" })
