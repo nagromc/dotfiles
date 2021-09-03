8a507afc4a2a89be2cf81c2e812aa1ea15aef6b6
Morgan Courbet <github@sub.ploki.fr>
2021-09-03 16:24:19 +0200

    Add shell level in starship

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -15,3 +15,7 @@ Invoke-Expression (&starship init powershell)
 
 # zoxide
 Invoke-Expression (& { $hook = if ($PSVersionTable.PSVersion.Major -ge 6) { 'pwd' } else { 'prompt' } (zoxide init powershell --hook $hook) -join "`n" })
+
+
+# count shell level
+$ENV:SHLVL = [int] $ENV:SHLVL + 1
