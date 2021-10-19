23ef0ebd136c6d20990757641f32a1f343e7e3aa
Morgan Courbet <github@sub.ploki.fr>
2021-10-19 14:57:42 +0200

    Add `ripgrep` completion for PowerShell

--- Documents/PowerShell/completions.ps1
+++ Documents/PowerShell/completions.ps1
@@ -2,3 +2,4 @@ Invoke-Expression (& { (just --completions powershell ) -join "`n" })
 Import-Module posh-git
 Invoke-Expression (& { (chezmoi completion powershell) -join "`n" })
 Invoke-Expression (& { (miniserve --print-completions powershell) -join "`n" })
+. $($(rg --shimgen-help | Select-String -Pattern "^\s*Target: '(.*)rg.exe'\s*$").matches[0].groups[1].value + "complete\_rg.ps1")
