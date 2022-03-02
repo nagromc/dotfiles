a4368eea1a9d99f0b6b7b36c2a09d59c1883aeb3
Morgan Courbet <github@sub.ploki.fr>
2022-03-02 17:44:15 +0100

    Fix `rg` completion

--- Documents/PowerShell/completions.ps1
+++ Documents/PowerShell/completions.ps1
@@ -2,4 +2,4 @@ Invoke-Expression (& { (just --completions powershell ) -join "`n" })
 Import-Module posh-git
 Invoke-Expression (& { (chezmoi completion powershell) -join "`n" })
 Invoke-Expression (& { (miniserve --print-completions powershell) -join "`n" })
-. $($(rg --shimgen-help | Select-String -Pattern "^\s*Target: '(.*)rg.exe'\s*$").matches[0].groups[1].value + "complete\_rg.ps1")
+. $Env:USERPROFILE\scoop\apps\ripgrep\current\complete\_rg.ps1
