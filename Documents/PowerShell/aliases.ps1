b7e32ec06db0156621b71db67d536508590ba07f
Morgan Courbet <github@sub.ploki.fr>
2022-03-02 16:37:51 +0100

    Add `lazydocker`

--- Documents/PowerShell/aliases.ps1
+++ Documents/PowerShell/aliases.ps1
@@ -2,4 +2,5 @@ New-Alias -Name cm -Value chezmoi
 New-Alias -Name d -Value docker
 New-Alias -Name g -Value git
 New-Alias -Name lzg -Value lazygit
+New-Alias -Name lzd -Value lazydocker
 New-Alias -Name list -Value Get-ChildItem
