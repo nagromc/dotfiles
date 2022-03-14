4ec242ec7a086e430cef14728127cd5bc1e97b55
Morgan Courbet <github@sub.ploki.fr>
2022-03-14 11:04:52 +0100

    Add Docker completion on Windows

--- Documents/PowerShell/completions.ps1
+++ Documents/PowerShell/completions.ps1
@@ -3,3 +3,4 @@ Import-Module posh-git
 Invoke-Expression (& { (chezmoi completion powershell) -join "`n" })
 Invoke-Expression (& { (miniserve --print-completions powershell) -join "`n" })
 . $Env:USERPROFILE\scoop\apps\ripgrep\current\complete\_rg.ps1
+Import-Module DockerCompletion
