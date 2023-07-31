102c28fa8e619317d1292fd40984ebb0bc475652
Morgan Courbet <github@sub.ploki.fr>
2023-07-31 09:53:49 +0200

    Disable `miniserve` completion in PowerShell
    Considered as a malware by some AV

--- Documents/PowerShell/completions.ps1
+++ Documents/PowerShell/completions.ps1
@@ -1,7 +1,7 @@
 Invoke-Expression (& { (just --completions powershell ) -join "`n" })
 Import-Module posh-git
 Invoke-Expression (& { (chezmoi completion powershell) -join "`n" })
-Invoke-Expression (& { (miniserve --print-completions powershell) -join "`n" })
+# Invoke-Expression (& { (miniserve --print-completions powershell) -join "`n" })
 . $Env:USERPROFILE\scoop\apps\ripgrep\current\complete\_rg.ps1
 Import-Module DockerCompletion
 glab completion -s powershell | Out-String | Invoke-Expression
