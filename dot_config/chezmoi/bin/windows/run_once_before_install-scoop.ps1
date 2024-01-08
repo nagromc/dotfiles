8b86c1b44c3a5bf74e0852bcde797fdd3fe490fc
Morgan Courbet <github@sub.ploki.fr>
2024-01-08 14:12:53 +0100

    Skip if Scoop is already installed

--- dot_config/chezmoi/bin/windows/run_once_before_install-scoop.ps1
+++ dot_config/chezmoi/bin/windows/run_once_before_install-scoop.ps1
@@ -1,3 +1,8 @@
+if (Get-Command "scoop" -ErrorAction SilentlyContinue)
+{
+  exit
+}
+
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 scoop install git
