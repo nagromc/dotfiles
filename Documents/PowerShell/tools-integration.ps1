0278998c7aa195b6ad40da9eca7f086fca3fd82c
Morgan Courbet <github@sub.ploki.fr>
2024-02-15 12:14:47 +0100

    Update zoxide integration in Powershell according to the latest docs

--- Documents/PowerShell/tools-integration.ps1
+++ Documents/PowerShell/tools-integration.ps1
@@ -5,7 +5,7 @@ Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 Invoke-Expression (&starship init powershell)
 
 # zoxide
-Invoke-Expression (& { $hook = if ($PSVersionTable.PSVersion.Major -ge 6) { 'pwd' } else { 'prompt' } (zoxide init powershell --hook $hook) -join "`n" })
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
 # add icons to `dir`
 # https://github.com/devblackops/Terminal-Icons must be installed with `scoop install terminal-icons`
