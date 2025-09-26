9693af1902582077534b844afb431f89f3b94f75
Morgan Courbet <github@sub.ploki.fr>
2025-09-26 15:29:42 +0200

    Move `YAZI_FILE_ONE` env var definition in system

--- .chezmoiscripts/windows/run_onchange_before_account-envvar.ps1
+++ .chezmoiscripts/windows/run_onchange_before_account-envvar.ps1
@@ -1,1 +1,2 @@
 [System.Environment]::SetEnvironmentVariable('KOMOREBI_CONFIG_HOME', '%USERPROFILE%\.config\komorebi', 'User')
+[System.Environment]::SetEnvironmentVariable('YAZI_FILE_ONE', '%USERPROFILE%\scoop\apps\git\current\usr\bin\file.exe', 'User')
