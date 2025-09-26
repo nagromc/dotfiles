074bcec463dad8f72cf3320692985a18e4891b8a
Morgan Courbet <github@sub.ploki.fr>
2025-09-26 15:15:54 +0200

    Fix Carapace for Windows PowerShell

--- Documents/WindowsPowerShell/Profile.ps1
+++ Documents/WindowsPowerShell/Profile.ps1
@@ -5,6 +5,6 @@ Invoke-Expression (&starship init powershell)
 . "$env:APPDATA/dystroy/broot/config/launcher/powershell/br.ps1"
 
 # carapace shell completer
-Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
+Set-PSReadLineOption -Colors @{ "Selection" = "$([char]0x1b)[7m" }
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 carapace _carapace | Out-String | Invoke-Expression
