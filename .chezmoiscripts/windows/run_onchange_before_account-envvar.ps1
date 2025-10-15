bd43e002defd62a2ead3ec856b69fabe45787a0d
Morgan Courbet <github@sub.ploki.fr>
2025-10-15 15:02:57 +0200

    Change `EDITOR` to `vim`

--- .chezmoiscripts/windows/run_onchange_before_account-envvar.ps1
+++ .chezmoiscripts/windows/run_onchange_before_account-envvar.ps1
@@ -1,2 +1,3 @@
+[System.Environment]::SetEnvironmentVariable('EDITOR', 'vim', 'User')
 [System.Environment]::SetEnvironmentVariable('KOMOREBI_CONFIG_HOME', '%USERPROFILE%\.config\komorebi', 'User')
 [System.Environment]::SetEnvironmentVariable('YAZI_FILE_ONE', '%USERPROFILE%\scoop\apps\git\current\usr\bin\file.exe', 'User')
