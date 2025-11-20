af791c9015b2fc8b92fb06ac38b91b31c0d4189c
Morgan Courbet <github@sub.ploki.fr>
2025-11-20 13:16:32 +0100

    pwsh: exit early with lanched with `-Command` / `-c`
    That prevent to initialize the entire profile when tools use pwsh
    to launch other external tools.

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -1,7 +1,19 @@
+function Test-ProfileIsRunWithCommand {
+  $cmdLine = (Get-CimInstance Win32_Process -Filter "ProcessId = $PID").CommandLine
+  return $cmdLine -match '\s-(Command|c)\s+'
+}
+
+
+
+. "$PSScriptRoot/envvars.ps1"
 . "$PSScriptRoot/path.ps1"
+
+if (Test-ProfileIsRunWithCommand) {
+  Write-Host "Skipped full PowerShell profile initialization" -ForegroundColor Yellow
+  return
+}
 
 . "$PSScriptRoot/aliases.ps1"
 . "$PSScriptRoot/completions.ps1"
-. "$PSScriptRoot/envvars.ps1"
 . "$PSScriptRoot/functions.ps1"
 . "$PSScriptRoot/tools-integration.ps1"
