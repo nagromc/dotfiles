7ca98b87b5d80d352f535e9e42c67a77612cec0f
Morgan Courbet <github@sub.ploki.fr>
2026-08-07 16:46:09 +0200

    Don’t exit immediately the init script if Scoop is already installed

--- .chezmoiscripts/windows/run_once_before_00-install-scoop.ps1
+++ .chezmoiscripts/windows/run_once_before_00-install-scoop.ps1
@@ -1,8 +1,7 @@
-if (Get-Command "scoop" -ErrorAction SilentlyContinue)
+if (-not (Get-Command "scoop" -ErrorAction SilentlyContinue))
 {
-  exit
+  Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+  Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 }
 
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 scoop install aria2 git sudo
