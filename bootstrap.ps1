c3cea55d1f7e697e79e4113bbabc5690b6ac09ad
Morgan Courbet <github@sub.ploki.fr>
2026-08-11 15:48:51 +0200

    Use variables in GitHub actions

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,3 +1,12 @@
+param(
+    [string]$BranchName = ""
+)
+
 $Env:Path += ";$Env:USERPROFILE\scoop\shims;$Env:USERPROFILE\scoop\apps\vscode\current\bin"  # add Scoop shims path temporarily during bootstrap
 
-Invoke-Expression "&{$(Invoke-RestMethod 'https://get.chezmoi.io/ps1')} -BinDir $Env:TEMP -- init --apply --verbose nagromc"
+$BranchArg = ""
+if ($BranchName) {
+    $BranchArg = "--branch $BranchName"
+}
+
+Invoke-Expression "&{$(Invoke-RestMethod 'https://get.chezmoi.io/ps1')} -BinDir $Env:TEMP -- init --apply --verbose $BranchArg nagromc"
